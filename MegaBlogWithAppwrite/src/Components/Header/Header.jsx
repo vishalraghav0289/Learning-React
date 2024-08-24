c1c9c1660deaa094c988e4821c2c92b802078abf
vishalraghav0289 <vishalraghav0289@gmail.com>
2024-08-24 23:38:14 +0530

    upadted with button component

--- MegaBlogWithAppwrite/src/Components/Header/Header.jsx
+++ MegaBlogWithAppwrite/src/Components/Header/Header.jsx
@@ -42,10 +42,26 @@ function Header() {
         <nav className='flex'>
           <div className='mr-4'>
             <Link to='/'>
-            <Logo/>
+            <Logo width='70px'/>
             </Link>
             
           </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item)=> item.active ? (
+              <li key={item.name}>
+                <button onClick={()=>navigate(item.slug)}
+                   className='inline-bock px-6 py-2 
+                   duration-200 hover:bg-blue-100 rounded-full'>
+                  {item.name}</button>
+              </li>
+            ) : null)}
+            {authStatus && (
+              <li>
+                <Logout />
+              </li>
+            )}
+
+            </ul>
         </nav>
 
       </Contaner>
