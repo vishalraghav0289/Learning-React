5d872ed5d0cc5932075d1abc0f00eeb583cda409
vishalraghav0289 <vishalraghav0289@gmail.com>
2024-08-31 00:03:55 +0530

    Added RTE & PostFormfile

--- MegaBlogWithAppwrite/src/Components/Index.js
+++ MegaBlogWithAppwrite/src/Components/Index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Contaner from './Contaner/Contaner'
 import Logout from "./Header/Logout";
 import Logo from "./Header/Logo"
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Contaner,
     Logo,
     Logout,
+    RTE,
 }
