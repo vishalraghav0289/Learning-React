76c0498852ee9803aee0d25738b4046be5396147
vishalraghav0289 <vishalraghav0289@gmail.com>
2024-09-02 21:11:58 +0530

    Added some more Pages

--- MegaBlogWithAppwrite/src/Components/Index.js
+++ MegaBlogWithAppwrite/src/Components/Index.js
@@ -1,14 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Contaner from './Contaner/Contaner'
-import Logout from "./Header/Logout";
-import Logo from "./Header/Logo"
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
-    Contaner,
+    Container,
     Logo,
-    Logout,
+    LogoutBtn,
     RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
